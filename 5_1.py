e4deb842c0e92a287e0e574fe83758368c5fc1a0
zhenrui1 <zhenrui1219@mail.fresnostate.edu>
2022-04-28 15:08:16 -0700

    addded some code

--- 5_1.py
+++ 5_1.py
@@ -2,27 +2,49 @@
 file_name = "./infile.txt"
 infile = open(file_name, "r")
 names = [line.rstrip() for line in infile]
-print("names: ", names)
+# print("names: ", names)
 
-infile.close()
+# infile.close()
+
+# def main():
+#     display_with_forloop(file_name)
+#     print()
+#     display_with_forloop()
+
+# def display_with_forloop(file_name):
+#     infie = open(file_name, "r")
+#     for line in infile:
+#         print(line, ends="")
+#     infile.closed()
+
+
+# def display_with_forloop(file_name):
+#     infile = open(file_name, "r")
+#     items = [line.rstrip() for line in infile]
+#     print("items: ", items)
+#     infile.closed()
+
+
+
+# def open_file(file_name, mode):
+#     return open(file_name, mode)
+
+# main()
 
 def main():
-    display_with_forloop(file_name)
-    print()
-    display_with_forloop()
+    save_to_outfile()
 
-def display_with_forloop(file_name):
-    infie = open(file_name, "r")
-    for line in infile:
-        print(line, ends="")
-    infile.closed()
+def open_file(file_name, mode):
+    return open(file_name, mode)
 
-
-def display_with_forloop(file_name):
-    infile = open(file_name, "r")
-    items = [line.rstrip() for line in infile]
-    print("items: ", items)
-    infile.closed()
-
+def save_to_outfile(file_name):
+    outfile = open_file("./outfile.txt", "w")    
+    for name in names:
+        if "Doe" not in name:
+            print("Name to be persisted: ", name) 
+            outfile.write(name + "\n")   
+    else:
+        print("Names to be exclude: ", name)
+    outfile.close()
 
 main()
